052bdef2e30dd1a4ca527e757d0b5fd6cc8ae938
Supuna <supunawa@gmail.com>
2022-03-27 23:58:32 +0530

    hero section added

--- pages/_app.js
+++ pages/_app.js
@@ -14,7 +14,7 @@ function MyApp({ Component, pageProps }) {
         <link rel="icon" href="/favicon.ico" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
-        <link href="https://fonts.googleapis.com/css2?family=Abril+Fatface&family=Josefin+Sans:wght@100;200;300;400;500;600;700&display=swap" rel="stylesheet" />
+        <link href="https://fonts.googleapis.com/css2?family=Abril+Fatface&family=Fredericka+the+Great&family=Josefin+Sans:wght@100;200;300;400;500;600;700&display=swap" rel="stylesheet" />
       </Head>
 
       <Header />
--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,18 @@
 import styles from '../styles/Home.module.css'
+import Image from "next/image"
+import Background from '../public/home_bg.jpg'
 
 export default function Home() {
   return (
     <div className={styles.container}>
-      <h1>Home Page</h1>
+      
+      <div className={`${styles.hero} px-5 py-5 text-center`}>
+        <h1 className={`${styles.title} display-5 fw-bold mt-md-5`}>Get it while it's hot!</h1>
+        <div className="col-lg-6 mx-auto">
+          <p className={`${styles.subTitle} lead mb-4 mt-md-5`}>We create custom, personal pizzas with quality ingredients in only five minutes.</p>
+        </div>
+      </div>
+
     </div>
   )
 }
