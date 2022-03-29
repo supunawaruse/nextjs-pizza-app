0df3bb278c655e0c936460efd1ac1d4773d9a223
Supuna <supunawa@gmail.com>
2022-03-29 11:20:26 +0530

    navbar modified

--- components/Header.js
+++ components/Header.js
@@ -1,47 +1,61 @@
 import Image from "next/image"
+import Link from "next/link"
 import logo from "../public/logo.png"
 import styles from "../styles/Header.module.css"
 
 const Header = () => {
   return (
-    <nav className={`${styles.header} navbar navbar-expand-sm px-3 px-md-5`}>
+    <nav className={`${styles.header} navbar navbar-expand-sm px-4 px-md-5`}>
       <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-        <span className="navbar-toggler-icon"></span>
+        <span className="navbar-toggler-icon"><i class="bi bi-justify" style={{color:'white',fontSize:24}}></i></span>
       </button>
       <div className="collapse navbar-collapse nav-mobile" id="navbarSupportedContent">
-        <ul className="navbar-nav me-auto d-flex d-sm-none">
+        <div className={styles.item}>
+          <div className={styles.callButton}>
+            <i class="bi bi-telephone-fill"></i>
+          </div>
+          <div className={styles.texts}>
+            <div className={styles.text}>ORDER NOW!</div>
+            <div className={styles.text}>+07 123 123</div>
+          </div>
+        </div>
+        <div className={styles.item}>
+        <ul className="navbar-nav me-auto d-flex align-items-md-center justify-content-center my-3 my-md-0">
+          <li className="nav-item d-sm-none">
+            <Link href="/">
+              <a className="nav-link active" aria-current="page" >
+                <Image src={logo} alt="" className="img-fluid" />
+              </a>
+            </Link>
+          </li>
           <li className="nav-item">
-            <a className="nav-link active" aria-current="page" href="#">
-              <Image src={logo} alt="" className="img-fluid" />
-            </a>
+            <Link href="/">
+              <a className={`${styles.nav_text} nav-link mx-md-2`}>HOME</a>
+            </Link>
+          </li>
+          <li className="nav-item">
+            <Link href="/menu">
+              <a className={`${styles.nav_text} nav-link mx-md-2`} >MENU</a>
+            </Link>
+          </li>
+          <li className="nav-item d-none d-md-block">
+            <Link href="/">
+              <a className="nav-link mx-1" href="#">
+                  <Image src={logo} alt="" className="img-fluid" />
+              </a>
+            </Link>
+          </li>
+          <li className="nav-item">
+              <a className={`${styles.nav_text} nav-link mx-md-2`} href="/">ABOUT</a>
+          </li>
+          <li className="nav-item">
+              <a className={`${styles.nav_text} nav-link mx-md-2`} href="/">CONTACT</a>
           </li>
         </ul>
-        <ul className="navbar-nav m-auto">
-          
-          <li className="nav-item">
-            <a className={`${styles.nav_text} nav-link`} href="/">HOME</a>
-          </li>
-          <li className="nav-item">
-            <a className={`${styles.nav_text} nav-link`} href="/menu">MENU</a>
-          </li>
-        </ul>
-  
-        <ul className="navbar-nav d-none d-sm-flex m-auto">
-          <li className="nav-item">
-            <a className="nav-link active" href="#">
-              <Image src={logo} alt="" className="img-fluid" />
-            </a>
-          </li>
-        </ul>
-  
-        <ul className="navbar-nav m-auto">
-            <li className="nav-item">
-                <a className={`${styles.nav_text} nav-link`} href="/">ABOUT</a>
-              </li>
-              <li className="nav-item">
-                <a className={`${styles.nav_text} nav-link`} href="/">CONTACT</a>
-              </li>
-        </ul>
+        </div>
+        <div className={`${styles.item} d-flex justify-content-sm-end`}>
+          <i className="bi bi-cart" style={{fontSize:20,color:'#fdbc2c'}}></i>
+        </div>
       </div>
   </nav>
   )
--- pages/index.js
+++ pages/index.js
@@ -6,10 +6,10 @@ import Pizza from '../public/2.png'
 export default function Home() {
   return (
     <div className={styles.container}>
-      <div className={`${styles.hero} px-5 pb-5 text-center`}>
-        <h1 className={`${styles.title} display-5 fw-bold mt-md-5`}>Get it while it's hot!</h1>
+      <div className={`${styles.hero} px-5 pb-3 text-center`}>
+        <h1 className={`${styles.title} display-5 fw-bold mt-md-4`}>Get it while it's hot!</h1>
         <div className="col-lg-6 mx-auto">
-          <p className={`${styles.subTitle} lead mb-4 mt-md-3`}>We create custom, personal pizzas with quality ingredients in only five minutes.</p>
+          <p className={`${styles.subTitle} lead mb-4 mt-md-2`}>We create custom, personal pizzas with quality ingredients in only five minutes.</p>
         </div>
         <button className={`${styles.order_button} btn`}>VIEW MENU</button>
       </div>
