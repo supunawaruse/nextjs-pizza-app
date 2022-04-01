47c0394ad329640af014f61acbacdfeac8347132
Supuna <supunawa@gmail.com>
2022-04-01 22:05:49 +0530

    about page completed

--- pages/about.js
+++ pages/about.js
@@ -45,12 +45,39 @@ const about = () => {
             <h1 className={styles.headerText}>Frequently Asked Questions</h1>
             <p className={styles.simpleText}>Here are the answers to the most common questions we receive.</p>
 
-            <div className='accordion'>
-                <div className='panel'>
-                    
+            <div className="accordion" id="accordionFlushExample">
+                <div className="accordion-item mb-2"style={{border:'none'}}>
+                  <h2 className="accordion-header" id="flush-headingOne">
+                    <button className={`${styles.accordion_title} accordion-button collapsed`} type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseOne" aria-expanded="false" aria-controls="flush-collapseOne">
+                      I cannot find what I am looking for. Where can I go to ask my question?
+                    </button>
+                  </h2>
+                  <div id="flush-collapseOne" className="accordion-collapse collapse" aria-labelledby="flush-headingOne" data-bs-parent="#accordionFlushExample">
+                    <div className={`${styles.accordion_body} accordion-body`}>We like to say deliveries take approximately 45 mins during weekdays and approximately 1 hour on weekends. We do our best to get your delicious pizza out to you as soon as possible (we know you are craving it) but we like to give ourselves a little extra time in case our drivers encounter unforeseen circumstances, which may include traffic and bad weather. The most important thing to us is making sure our drivers arrive safely and that your pizza arrives hot!</div>
+                  </div>
+                </div>
+                <div className="accordion-item mb-2"style={{border:'none'}}>
+                  <h2 className="accordion-header" id="flush-headingTwo">
+                    <button className={`${styles.accordion_title} accordion-button collapsed`} type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseTwo" aria-expanded="false" aria-controls="flush-collapseTwo">
+                    What is the minimum amount I have to order for delivery?
+                    </button>
+                  </h2>
+                  <div id="flush-collapseTwo" className="accordion-collapse collapse" aria-labelledby="flush-headingTwo" data-bs-parent="#accordionFlushExample">
+                    <div className={`${styles.accordion_body} accordion-body`}>We like to say deliveries take approximately 45 mins during weekdays and approximately 1 hour on weekends. We do our best to get your delicious pizza out to you as soon as possible (we know you’re craving it) but we like to give ourselves a little extra time in case our drivers encounter unforeseen circumstances, which may include traffic and bad weather. The most important thing to us is making sure our drivers arrive safely and that your pizza arrives hot!</div>
+                  </div>
+                </div>
+                <div className="accordion-item"style={{border:'none'}}>
+                  <h2 className="accordion-header" id="flush-headingThree">
+                    <button className={`${styles.accordion_title} accordion-button collapsed`} type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseThree" aria-expanded="false" aria-controls="flush-collapseThree">
+                    How long will my delivery take?
+                    </button>
+                  </h2>
+                  <div id="flush-collapseThree" className="accordion-collapse collapse" aria-labelledby="flush-headingThree" data-bs-parent="#accordionFlushExample">
+                    <div className={`${styles.accordion_body} accordion-body`}>We like to say deliveries take approximately 45 mins during weekdays and approximately 1 hour on weekends. We do our best to get your delicious pizza out to you as soon as possible (we know you’re craving it) but we like to give ourselves a little extra time in case our drivers encounter unforeseen circumstances, which may include traffic and bad weather. The most important thing to us is making sure our drivers arrive safely and that your pizza arrives hot!</div>
+                  </div>
                 </div>
             </div>
-    </div>
+      </div>
 
   </div>
   </div>
